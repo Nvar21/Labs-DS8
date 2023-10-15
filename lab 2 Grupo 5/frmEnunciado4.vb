29c9c1a37db8d9d814752e0df70fb958035ce430
Iyaa <iyaaniris.arevalo@utp.ac.pa>
2023-10-15 00:39:06 -0500

    Enunciado 4, revisando

--- lab 2 Grupo 5/frmEnunciado4.vb
+++ lab 2 Grupo 5/frmEnunciado4.vb
@@ -1,7 +1,11 @@
 ﻿Public Class frmEnunciado4
     Private Sub BtnCalcular_Click(sender As Object, g As EventArgs) Handles btnCalcular.Click
+        'Variable para almacenar el año ingresado.
         Dim year As Integer
-        If Integer.TryParse(txtYear.Text, year) AndAlso year >= 1986 And year <= 2100 Then
+
+        ' Validar si el año ingresado es un número válido
+        ' (Esta fórmula y sus coeficientes 24 y 5 son válidos desde el año 1900 hasta el 2100.)
+        If Integer.TryParse(txtYear.Text, year) AndAlso year >= 1900 And year <= 2100 Then
 
             Dim A As Integer = year Mod 19
             Dim B As Integer = year Mod 4
@@ -10,6 +14,7 @@
             Dim E As Integer = (2 * B + 4 * C + 6 * D + 5) Mod 7
             Dim N As Integer = 22 + D + E
 
+            'Comprobar si la fecha cae en marzo o abril
             If N <= 31 Then
                 MsgBox("La fecha de Pascua en el año " & year & " es el Domingo " & N & " de marzo.", MsgBoxStyle.Information, "Mensaje")
             Else N -= 31
