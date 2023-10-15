a45ee3b662dbc9e37e1b108a90bcfaef3bbe19fb
Nvar21 <neja200211@gmail.com>
2023-10-14 21:15:06 -0500

    enunciado 3 creo que listo ya

---
+++ lab 2 Grupo 5/ModuleEnun3.vb
@@ -0,0 +1,18 @@
+﻿Module ModuleEnun3
+    Function Inversion(numeroComoCadena As String) As String
+        ' Caso base: si la cadena está vacía o tiene una sola cifra, regresamos la cadena tal como está
+        If String.IsNullOrEmpty(numeroComoCadena) OrElse numeroComoCadena.Length = 1 Then
+            Return numeroComoCadena
+        End If
+
+        ' Obtenemos el último carácter de la cadena
+        Dim ultimoCaracter As Char = numeroComoCadena(numeroComoCadena.Length - 1)
+
+        ' Llamada recursiva con la cadena sin el último carácter
+        Dim cadenaRestante As String = numeroComoCadena.Substring(0, numeroComoCadena.Length - 1)
+        Dim cadenaInvertidaRestante As String = Inversion(cadenaRestante)
+
+        ' Concatenamos el último carácter al resultado invertido
+        Return ultimoCaracter & cadenaInvertidaRestante
+    End Function
+End Module
--- lab 2 Grupo 5/frmEnunciado3.vb
+++ lab 2 Grupo 5/frmEnunciado3.vb
@@ -1,3 +1,28 @@
 ﻿Public Class frmEnunciado3
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles txtNumero.TextChanged
+        ' Obtén el número entero del cuadro de texto (suponiendo que se ingrese un número válido)
+        Dim numeroOriginal As Integer
+        If String.IsNullOrEmpty(txtNumero.Text) Then
+            lblPrueba.Show()
+            lblPrueba.Text = "No puede dejar vacío campo de número"
+            lblPrueba.ForeColor = Color.FromArgb(233, 213, 2) 'Amarillo de Alerta
+            txtResultado.Text = ""
+        Else
+            If Integer.TryParse(txtNumero.Text, numeroOriginal) Then
+                lblPrueba.Show()
+                lblPrueba.Text = "Valor Válido"
+                lblPrueba.ForeColor = Color.FromArgb(153, 204, 51) 'Verde de Correcto
+                ' Llama a la función del modulo para invertir el número
+                Dim numeroInvertido As Integer = Integer.Parse(Inversion(numeroOriginal.ToString()))
+                ' Muestra el número invertido en un cuadro de texto
+                txtResultado.Text = numeroInvertido.ToString()
+            Else ' Manejo de errores si la entrada no es un número válido
+                lblPrueba.Show()
+                lblPrueba.Text = "Tipo de dato Incorrecto, no ingrese letras o caracteres"
+                txtResultado.Text = ""
+                lblPrueba.ForeColor = Color.FromArgb(255, 153, 102) 'Rojo de Incorrecto
+            End If
+        End If
 
+    End Sub
 End Class
