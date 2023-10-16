78c3c6eb3b74cf58cb42969b2f4b45acfd58ea13
Iyaa <iyaaniris.arevalo@utp.ac.pa>
2023-10-15 23:56:14 -0500

    Enunciado 4

--- lab 2 Grupo 5/Modulo.vb
+++ lab 2 Grupo 5/Modulo.vb
@@ -12,25 +12,25 @@
 
 
     'funcion para enunciado 4
-    Function CalcularDomingo(ByVal year As Integer) As DateTime
+    Function CalcularDomingo(ByVal year As Integer) As String
         Dim A As Integer = year Mod 19
         Dim B As Integer = year Mod 4
         Dim C As Integer = year Mod 7
         Dim D As Integer = (19 * A + 24) Mod 30
         Dim E As Integer = (2 * B + 4 * C + 6 * D + 5) Mod 7
         Dim N As Integer = 22 + D + E
-
-        Dim fechaPascua As DateTime
-
+        Dim fechaPascua As String
         If N <= 31 Then
-            'Domingo de pascua Marzo
-            fechaPascua = New DateTime(year, 3, N)
-        Else N -= 31
-            'Domingo de pascua Marzo
-            fechaPascua = New DateTime(year, 4, N)
+            ' Domingo de Pascua en marzo
+            fechaPascua = $"{N} de marzo"
+        Else
+            N -= 31
+            ' Domingo de Pascua en abril
+            fechaPascua = $"{N} de abril"
         End If
         Return fechaPascua
     End Function
+
 
 
     'funcion para enunciado 5
--- lab 2 Grupo 5/frmEnunciado4.vb
+++ lab 2 Grupo 5/frmEnunciado4.vb
@@ -1,17 +1,13 @@
 ﻿Public Class frmEnunciado4
     Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
-        'Variable para almacenar el año ingresado.
-        Dim year As Integer = Integer.Parse(txtYear.Text)
-
-        ' Validar si el año ingresado es un número válido
-        ' (Esta fórmula y sus coeficientes 24 y 5 son válidos desde el año 1900 hasta el 2100.)
+        Dim year As Integer
         If Integer.TryParse(txtYear.Text, year) AndAlso year >= 1900 And year <= 2100 Then
-            ' El resultado se almacena en "fechaPascua"
-            Dim fechaPascua As DateTime = CalcularDomingo(year)
-            MsgBox("El domingo de Pascua en el año " & year & " es el " & fechaPascua.ToString("d 'de' MMMM", New System.Globalization.CultureInfo("es-ES")), MsgBoxStyle.Information, "MENSAJE")
+            Dim fechaPascua As String = CalcularDomingo(year)
+            MsgBox("El domingo de Pascua en el año " & year & " es el " & fechaPascua, MsgBoxStyle.Information, "MENSAJE")
         Else
             MsgBox("Por favor, ingrese un año válido.", MsgBoxStyle.Information, "ERROR")
         End If
     End Sub
 End Class
+
 
