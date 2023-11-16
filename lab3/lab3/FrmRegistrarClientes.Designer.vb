f206bf1c6ef35ed780a080ae5ce415e39a2e59fd
Nvar21 <neja200211@gmail.com>
2023-11-16 17:35:21 -0500

    prueba de mi rama jejejeejejej

--- lab3/lab3/FrmRegistrarClientes.Designer.vb
+++ lab3/lab3/FrmRegistrarClientes.Designer.vb
@@ -23,23 +23,37 @@ Partial Class FrmRegistrarClientes
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(267, 58)
+        Me.Label1.Location = New System.Drawing.Point(200, 47)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(77, 16)
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "REGISTRO"
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(269, 177)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(171, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Queria probar lo de push a mi rama"
+        '
         'FrmRegistrarClientes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FrmRegistrarClientes"
         Me.Text = "FrmRegistrarClientes"
         Me.ResumeLayout(False)
@@ -48,4 +62,5 @@ Partial Class FrmRegistrarClientes
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
